2b5e5bed58d22a057873a93076c7b5db4fe483e3
Arthur Carvalho <arthur@Ubuntu22.04>
2024-11-24 20:40:46 -0300

    alteração

--- thread_race/thread_functions.c
+++ thread_race/thread_functions.c
@@ -4,7 +4,6 @@ pthread_t thread[THREADS];
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 int ready_count = 0; 
-int winner[3] = {0, 0, 0};
 int count = 0;
 
 void createThreads(){
@@ -28,7 +27,7 @@ void threadRace(void *arg){
 
     pthread_mutex_lock(&mutex);
     ready_count++;
-    if (ready_count == THREADS) {
+    if(ready_count == THREADS){
         pthread_cond_broadcast(&cond);
     } else {
         pthread_cond_wait(&cond, &mutex);
@@ -41,7 +40,6 @@ void threadRace(void *arg){
 
     pthread_mutex_lock(&mutex);
     if(count < 3){
-        winner[count] = thread_id;
         count++;
         printf("A thread %d ficou em %dº lugar.\n", thread_id, count);
     }
